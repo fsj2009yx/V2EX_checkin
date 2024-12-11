9a4d1c2110ab2c540240cb9a79d5567e1ca87948
fsj2009yx <148683018+fsj2009yx@users.noreply.github.com>
2024-12-11 15:49:19 +0800

    重构史

--- V2EX.py
+++ V2EX.py
@@ -3,94 +3,68 @@ from DrissionPage import ChromiumOptions
 import json
 import ddddocr
 
-#获取url
+# 获取url
 with open('config.json', 'r') as file:
-    json_str=json.load(file)#读取json文件并转成string列表
-    
-#浏览器配置
-co=ChromiumOptions()
-co.incognito()  # 匿名模式
-co.headless()  # 无头模式
+    json_config = json.load(file)  # 读取json文件并转成string列表
 
+# 浏览器配置
+co = ChromiumOptions()
+co.headless()
 page = ChromiumPage(co)
-page.get(json_str['url'])
-ele_parents = page.eles(".cell")[-1]#输入栏父节点
-
-#输入user
-def input_user():
-    ele1=ele_parents.ele("text:用户名")
-    ele2=ele1.next()
-    ele_input=ele2.child()
-    if(ele_input):
-        ele_input.input(json_str['name'])
-
-#输入密码
-def input_password():
-    ele1=ele_parents.eles("text:密码")[0]
-    ele2=ele1.next()
-    ele_input=ele2.child()
-    if(ele_input):
-        ele_input.input(json_str['password'])
-
-#验证码处理
-def input_Code():
-    ele1=ele_parents.eles("text:机器人")[0]
-    ele2=ele1.next()
-    ele_input=ele2.children()[2]
-    captcha_image=ele2.ele("#captcha-image")
-    if(captcha_image):
-        #元素#captcha-image截图
-        captcha_image.get_screenshot()
-        bytes_str = captcha_image.get_screenshot(as_bytes='png')
-        #识别验证码
-        ocr=ddddocr.DdddOcr()
-        Code=ocr.classification(bytes_str)
-        ele_input.input(Code)
 
 def login():
-    ele1 = ele_parents.eles("text:机器人")[0]
-    ele2=ele1.parent()
-    ele3=ele2.next()
-    ele4=ele3.children()[-1]
-    ele_login=ele4.children()[-1]
-    ele_login.click()
+    ele_cell = page.eles(".cell")[-1]#输入栏父节点
+    ele_user=ele_cell.ele("text:用户名").next().child()
+    ele_password=ele_cell.ele("text:密码").next().child()
+    ele_user.input(json_config['name'])
+    ele_password.input(json_config['password'])
+    input_Code(ele_cell)
 
-#签到
+# 验证码处理
+def input_Code(ele_cell):
+    ele2 = ele_cell.ele("text:机器人").next()
+    ele_input = ele2.children()[2]
+    captcha_image = ele2.ele("#captcha-image")
+    if (captcha_image):
+        # 元素#captcha-image截图
+        captcha_image.get_screenshot()
+        bytes_str = captcha_image.get_screenshot(as_bytes='png')
+        # 识别验证码
+        ocr = ddddocr.DdddOcr()
+        Code = ocr.classification(bytes_str)
+        ele_input.input(Code)
+
+    ele_cell.ele(".super normal button").click()
+
+# 签到
 def check_in():
-    ele_check=page.ele("text:领取今日的登录奖励")
+    ele_check = page.ele("text:领取今日的登录奖励")
     ele_check.click()
     page.wait.load_start()
-    ele1=page.ele("text:每日登录奖励")
-    ele_coin=ele1.next()
+    ele1 = page.ele("text:每日登录奖励")
+    ele_coin = ele1.next()
     ele_coin.click()
     page.wait.load_start()
 
-
 def main():
-    global ele_parents
-    input_password()
-    input_Code()
-    login()
-    page.wait.load_start()
-    ele_problem=page.eles(".problem")
-    if ele_problem:
-        print(ele_problem)
-        ele_parents=page.eles(".cell")[-1]  # 输入栏父节点
-        main()
-    else:
-        page.wait.load_start()
-        ele_failed=page.ele("text:登录受限")
-        if not ele_failed:
-            check_in()
-        else:
-            print(ele_failed)
-            print("访问受限：请更换网络代理或稍后再次尝试运行该脚本")
-
-
-
-if __name__ == '__main__':
+    page.get(json_config['base_url'])
     try:
-        input_user()
-        main()
-    finally:
+        check_in()
+        print("签到成功，正在关闭浏览器")
         page.close()
+    except Exception as e:
+        print("未登录状态")
+        page.get(json_config['url_login'])
+        try:
+            while page.url != json_config['base_url']:
+                login()
+            if page.url == json_config['base_url']:
+                print("登录成功，cookie已保存")
+                check_in()
+        except Exception as e:
+            print("登录失败")
+            return
+
+
+if __name__ == "__main__":
+    main()
